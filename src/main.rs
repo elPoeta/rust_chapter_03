0c35e091243c2728a906e21ff5e2ca6164df393f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-14 18:53:56 -0300

    variable mutability and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    // CONSTANTS
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds is: {THREE_HOURS_IN_SECONDS}");
+
+    // INMUTABLE
+    let i = 9;
+    println!("The value of i is: {i}");
+
+    // MUTABLE
+    let mut k = 5;
+    println!("The value of k is: {k}");
+    k = 6;
+    println!("The value of k is: {k}");
+
+    // SHADOWING
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    // VALID
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("{spaces}");
+
+    /* COMPILE-TIME ERROR
+
+    let mut spaces = "   ";
+    spaces = spaces.len();
+
+    */
 }
