892932368d7a639de5df3a2f4261046013b8baa6
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-14 19:44:02 -0300

    rust functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("main function!");
+
+    //  Statements are instructions that perform some action and do not return a value.
+    // Expressions evaluate to a resultant value. Let’s look at some examples.
+
+    // one statement
+    let y = 6;
+
+    //expression
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
